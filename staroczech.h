59f6a307b196e7916911ba15c88e0f879f10e892
roudnas <dominik@roudny.eu>
2021-12-16 01:48:01 +0100

    header: First translations

---
+++ staroczech.h
@@ -0,0 +1,98 @@
+/* 
+* Basically mapping czech.h to Old Czech.
+* Go visit: https://github.com/Toaster192/czech.h
+*
+*/
+
+
+// Data types
+#define SOUBOR FILE
+#define plavati float
+#define dvojduchý double
+#define číslo int
+#define karakter char
+#define kar char
+#define podlúhovatý long
+#define kracšie short
+#define kracši short
+#define znaménce signed
+#define bezznaménce unsigned
+#define prázdn void
+#define prostrano void
+#define spravedlenstvie bool
+#define pravdivost bool
+
+// Advanced data types 
+#define velikost_t size_t
+#define množnost_t size_t
+#define číslo8_t int8_t
+#define číslo16_t int16_t
+#define číslo32_t int32_t
+#define číslo64_t int64_t
+#define nčíslo8_t uint8_t
+#define nčíslo16_t uint16_t
+#define nčíslo32_t uint32_t
+#define nčíslo64_t uint64_t
+
+// logické konstrukce
+#define kdyžť if
+#define jestli if
+#define jinako else
+#define obak else
+#define doňadž while
+#define doněž while
+#define próvod for
+#define hotoviti do
+#define napínač switch
+#define případ case
+#define obecný default
+#define rozbiti break
+#define prodliti continue
+
+// ostatní klíčová slova
+#define vrátiti return
+#define stálý static
+#define trpné const
+#define trpný const
+#define tuhý const
+#define tržeti goto
+#define velikost(x) sizeof(x)
+#define množnost(x) sizeof(x)
+
+#define najpřědní main
+#define úhlavní main
+
+// Operators
+#define roveň ==
+#define rovnati ==
+#define rovnánie ==
+#define nýrovnanie !=
+#define nerovně !=
+#define ný !
+
+// Block controllers
+#define začětie {
+#define končenie }
+
+// File access permissions 
+#define PŘĚČITÁNIE "r"
+#define ZÁPIS "w"
+#define PŘÍPIS "a"
+#define DVOJNÉ_PŘEČITÁNIE "rb"
+#define DVOJNÝ_ZÁPIS "wb"
+#define DVOJNÝ_PŘÍPIS "wa"
+
+// standardní I/O streamy
+#define obyčejvstup stdin
+#define obyčejvýstupenie stdout
+#define obyčejvýstup stdout
+#define obyčejblud stderr
+
+// konstanty
+#define pravdicě 1
+#define pravda 1
+#define klam 0
+#define nýpravda 0
+#define nepravda 0
+#define KŘ '\n'
+#define NULA NULL
